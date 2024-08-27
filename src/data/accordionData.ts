66fd7195485cd51738d2d61441017847d5bf770b
Cykix <113121320+2021a1r057@users.noreply.github.com>
2024-08-27 17:46:04 +0530

    Update accordionData.ts

--- src/data/accordionData.ts
+++ src/data/accordionData.ts
@@ -3,7 +3,6 @@ export const accordionData =[
     "title": "Project",
     "content": "This project demonstrates my expertise in developing a fully functional and interactive tarot reading website. Designed to be both engaging and user-friendly, it features dynamic tarot readings, a crystal healing section, and a seamless booking system. This project showcases my ability to integrate modern technologies like React and TypeScript to deliver a polished, professional application."
   },
-  {
 {
   "title": "Authors",
   "content": "Aditya Zalpuri, Ayush Thakur, Ananya Thakur, Raghav Sharma"
